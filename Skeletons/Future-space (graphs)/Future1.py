1836a305b2d0b21edbdc61e077dc6727f19a5d71
Rosaline Snoeys <rosaline.snoeys@student.kuleuven.be>
2024-10-03 17:51:13 +0200

    graph challenges for future!

---
+++ Skeletons/Future-space (graphs)/Future1.py
@@ -0,0 +1,70 @@
+"""
+After an apocalypse obliterated much of the earth, you were forced to leave the planet using the brand new train-themed spaceship you bought off the NMBS 4 years ago. Naturally, people thought you were stupid for purchasing such an unnecessary vehicle but you of course are having the last laugh now...
+However, because your space-navigation system was made by ChatGPT 4 it's a little bit broken. In order to find your way to other civilizations on other planets, you first need to figure out what the spacemap would even look like! All you have now are a few random pieces of submaps which are way too confusing to use...
+
+On top of this, it is possible the nodes in your submaps are identical, but have a different distance attached to them. To play it safe, you decide to be pessimistic and choose the biggest distance.
+As if it wasn't hard enough to figure it out, some of the names in the subgraphs are not even complete... 
+In this exercise you will generate the correct graph given a set amount of sub-graphs.
+
+Note: this is a directed graph.
+Note: you can assume every planetname is written fully at least once!
+Note: it might be helpful to define multiple functions so you don't get spaghetticode
+
+A list of n subgraphs will be given to you in a list; a graph structure looks like this:
+{'AAA': {'B': 10},
+    'A': {'B': 40, 'A*A': 10},
+    'X': {'Y': 20, 'Y': 10},
+    'MM': {'X': 10},
+    'A': {'XX': 10},
+    'V': {'XYZ': 4}
+    'XY*' : {'V':5}
+    'V' : {'A' : 10}
+    }
+
+""" 
+subgraphs = [
+   {
+    'Kepler': {'Lilia': 10},
+    'Solares': {'Ruxae': 40, 'Pol*ro': 10},
+    'Vula*us': {'Lilia': 20, 'Stevon': 10},
+    'Stevon': {'Zithea': 10},
+    'Ulrone': {'Tor*s': 10},
+    'Ruxae': {'Ulrone': 4},
+    'Enov' : {'Zaturn':5},
+    'Polaro' : {'Za**lan' : 10},
+    }
+, 
+    {
+    'Kep**r': {'Dalea': 5, 'Enov': 10},
+    'Guru': {'S*lares': 60},
+    'Torus': {'Vularus': 5},
+    'Zatalan': {'Solares': 30},
+    'Guru': {'Solares': 60},
+    'Lilia': {'Guru': 20}
+    }, 
+    {
+    'Zithea': {'Polar*': 5},
+    'Ulrone': {'Torus': 15},
+    'Ruxae': {'Vularus': 10, 'Ulrone': 8},
+    'Torus': {'Ke*ler': 15},
+    'Enov' : {'Ulrone':20},
+    'Po*aro': {'Zatalan': 5, 'Vu*arus': 30},
+    'Vularus': {'Polaro': 30}
+    }
+]
+''' solution thought process:
+
+    1. just make a full easy graph with the wrong names.
+        This will generate a full graph, that sees incomplete names as seperate planets
+
+    2. find all the real planet names and incomplete planet names and match the incomplete ones to the complete ones, alter the graph accordingly
+
+    3. this is an easy problem, but might result in difficult code
+'''
+def combine_subgraphs(subgraphs):
+    pass
+
+
+
+
+
---
+++ Skeletons/Future-space (graphs)/Future3.py
@@ -0,0 +1,42 @@
+"""
+You still know what your solar system looks like!
+{
+    'Kepler': {'Lilia': 10,'Dalea': 5, 'Enov': 10},
+    'Solares': {'Ruxae': 40, 'Polaro': 10},
+    'Vularus': {'Lilia': 20, 'Stevon': 10,'Polaro': 30},
+    'Stevon': {'Zithea': 10},
+    'Ulrone': {'Torus': 15},
+    'Ruxae': {'Vularus': 10, 'Ulrone': 8},
+    'Enov' : {'Zaturn':5, 'Ulrone':20}
+    'Polaro' : {'Zatalan' : 10, 'Vularus': 30},
+    'Torus': {'Kepler': 15, 'Vularus': 5},
+    'Guru': {'Solares': 60},
+    'Zatalan': {'Solares': 30},
+    'Lilia': {'Guru': 20},
+    'Zithea': {'Polaro': 5}
+}
+
+But you forgot one really important bit! YOU HAVE A FUELMAKER IN YOUR SPACESHIP.
+It is now your job to figure out whether your trip is possible, knowing you regenerate an x amount of litres of fuel after spending y litres of fuel
+
+Note: the input will look like this:
+
+You return a bool (True/False)
+graph = graph like in the example
+start = start destination (string)
+end = end destination (string)
+fuel = int
+regen = [amount of fuel you get, amount of fuel you spend]
+
+Note: the output should be a boolean
+
+Note: This is an example situation of what is ***NOT*** possible
+    tank: 30l
+    next_dest: 40l
+    getback-after-travelled: [15l, 20l]
+        -> "I can travel to my next destination because I have 30 litres in my tank, i can just travel for 20l, then i have 10 left but remake 15 so i have 20l left and therefore i can travel with my remaining 20l and arrive at my destination with 5l still in my tank!"
+    (to be clear, you must have the 40litres in your tank to go to the next destination, you can't generate fuel while travelling. Only after you have traveled, you get the extra fuel back)
+
+""" 
+def travel(graph,start,end,fuel,regen):
+    pass
